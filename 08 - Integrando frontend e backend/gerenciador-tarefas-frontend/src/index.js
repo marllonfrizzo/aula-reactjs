af5fa05340ce611c5d8fec5bb2dea039eaa6d68d
Marllon Frizzo <mffrizzo@gmail.com>
2018-07-11 13:11:31 -0300

    Tela de cadastro de usuário

--- 08 - Integrando frontend e backend/gerenciador-tarefas-frontend/src/index.js
+++ 08 - Integrando frontend e backend/gerenciador-tarefas-frontend/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
 import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "http://localhost:3001/api/";
 
-const index = () => {
+const Index = () => (
     <BrowserRouter>
         <App />
     </BrowserRouter>
-}
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
